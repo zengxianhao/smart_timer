769f5130c3a2a0b6779a3a2333800b6629e71e33
zengxianhao <418778645@qq.com>
2018-07-30 22:55:24 +0800

    新增目录 by zengxiaolong 20180730

---
+++ 每周_1-5_开始时间_7_00_结束时间_7_30_定时播放_古诗/raspberry_start_up.py
@@ -0,0 +1,8 @@
+#!/bin/bash
+# -*- coding: UTF-8 -*-
+# This is a very simple example
+
+
+current_time=`date "+%Y-%m-%d %H:%M:%S"`
+
+
---
+++ 每周_1-5_开始时间_7_00_结束时间_7_30_定时播放_古诗/read_crontab_time.py
@@ -0,0 +1,37 @@
+#coding:utf-8
+import os
+import re
+
+from sys import stdin
+from os import walk
+from os.path import join
+
+
+target_string = "定时播放"
+crontab_file  = "/etc/crontab"
+
+# 获取crontab中的原始内容
+file_handle = open(crontab_file, 'rb')
+for line in file_handle.readlines():
+    line = line.strip()
+
+    result = re.search(target_string, line)
+    if result:
+        #print line
+        smart_timer_dir = line.split()[7]
+        smart_timer_cmd = smart_timer_dir.split("/")[4]
+        smart_timer_cmd_split = smart_timer_cmd.split("_")
+        #print "%s" %(smart_timer_cmd_split)
+        smart_timer_cmd_split[1] = smart_timer_cmd_split[1].replace('-', '到')
+        smart_timer_cmd_split[1].replace('-', '到')
+        read_format = "%s %s %s %s:%s %s %s:%s %s %s文件" %(smart_timer_cmd_split[0], smart_timer_cmd_split[1], \
+                                                        smart_timer_cmd_split[2], smart_timer_cmd_split[3], \
+                                                        smart_timer_cmd_split[4], smart_timer_cmd_split[5], \
+                                                        smart_timer_cmd_split[6], smart_timer_cmd_split[7], \
+                                                        smart_timer_cmd_split[8], smart_timer_cmd_split[9])
+        #print read_format
+        read_cmd = "python3 baidu_tts.py \"%s\"" %(read_format)
+        os.system(read_cmd)
+
+file_handle.close()
+    
