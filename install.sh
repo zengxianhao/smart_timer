29835008dde47d6e3fabb4d64d081a65b864914a
zengxianhao <418778645@qq.com>
2018-08-30 22:19:18 +0800

    修改安装脚本

--- install.sh
+++ install.sh
@@ -4,11 +4,10 @@ chmod -R 755 *
 
 current_dir=`pwd`
 
+# 删除原来的定时制作脚本
+sed -i '/do_smart_timer.sh/d' /etc/crontab
+
+# 添加新的定时器制作脚本
 do_smart_timer="*/5 *   * * *   root    cd $current_dir && ./do_smart_timer.sh"
+sudo echo "$do_smart_timer" >> /etc/crontab
 
-grep "$do_smart_timer" /etc/crontab
-
-if [[ $? != 0 ]]
-then
-    sudo echo "$do_smart_timer" >> /etc/crontab
-fi
