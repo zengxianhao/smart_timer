e90ad9b9c2f144c3bf589f99e5a5acee5acce74e
zengxianhao <418778645@qq.com>
2018-08-31 20:33:03 +0800

    修改install.sh，增加安装播放器命令

--- install.sh
+++ install.sh
@@ -4,6 +4,9 @@ chmod -R 755 *
 
 current_dir=`pwd`
 
+echo "install omxplayer"
+apt-get install omxplayer
+
 echo "config /etc/crontab"
 
 # 备份定时配置文件
@@ -16,7 +19,6 @@ sed -i '/do_smart_timer.sh/d' /etc/crontab
 do_smart_timer="*/5 *   * * *   root    cd $current_dir && ./do_smart_timer.sh"
 echo "$do_smart_timer" >> /etc/crontab
 
-#python control_electric_raley.py 1
 # 设置定时器任务
 python modify_etc_crontab.py
 # 重新启动定时器任务
@@ -24,11 +26,4 @@ python modify_etc_crontab.py
 
 echo "Smart Timer Install OK!"
 
-#python3  baidu_tts.py  "定时播放闹钟如下：好"
-# 根据crontab文件播报定时闹钟时间
-#python read_crontab_time.py
-
-#python3  baidu_tts.py  "您好，树梅派定时闹钟设置完毕，再见，好"
-
-#python control_electric_raley.py 0
 
